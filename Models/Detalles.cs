570d77d86ef89367f6a8c825f030a7bda55c06d2
Ramon Gonzalez <ramon_gonzalez2@ucne.edu.do>
2025-07-07 19:21:37 -0400

    Agregando componentes necesarios

---
+++ Models/Detalles.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Ramon_Gonzalez_AP1_P2.Models
+{
+    public class Detalles
+    {
+
+        [Key]
+        public int Id { get; set; }
+    }
+}

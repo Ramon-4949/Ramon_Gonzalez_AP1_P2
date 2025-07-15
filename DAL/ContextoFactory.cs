c0977f78a20247833998e018d5a3462ae227e16e
Ramon Gonzalez <ramon_gonzalez2@ucne.edu.do>
2025-07-14 20:32:01 -0400

    Agregando componentes necesarios

--- DAL/ContextoFactory.cs
+++ DAL/ContextoFactory.cs
@@ -1,6 +1,18 @@
-﻿namespace Ramon_Gonzalez_AP1_P2.DAL
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Ramon_Gonzalez_AP1_P2.Context;
+
+
+namespace Ramon_Gonzalez_AP1_P2.Context
 {
-    public class ContextoFactory
+    public class ContextoFactory : IDesignTimeDbContextFactory<Contexto>
     {
+        public Contexto CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<Contexto>();
+            optionsBuilder.UseSqlServer("workstation id=Parcial2AP1.mssql.somee.com;packet size=4096;user id=Elpiord49_SQLLogin_1;pwd=qiyxavz7hf;data source=Parcial2AP1.mssql.somee.com;persist security info=False;initial catalog=Parcial2AP1;TrustServerCertificate=True");
+
+            return new Contexto(optionsBuilder.Options);
+        }
     }
 }
